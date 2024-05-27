0aef0c7a123ab544265c6429f9a4dedad325d61a
Yoshua19 <f4r3ver@gmail.com>
2024-05-26 19:33:16 -0500

    Se realizo una actualizacion en el Javascript (cambio del anchoPagina y el resize que detectaba el codigo windows)

--- public_html/js/login_signup.js
+++ public_html/js/login_signup.js
@@ -1,9 +1,8 @@
 /*Voy a ejecutar el click*/ /*En la función register*/
 document.getElementById("btn_registrarse").addEventListener("click", register);
-document
-  .getElementById("btn_iniciarSesion")
-  .addEventListener("click", iniciarSesion);
-window.addEventListener("resize", anchoPagina);
+//Cuando se haga clic en el boton iniciarSesion, se cambiara de pantalla
+document.getElementById("btn_iniciarSesion").addEventListener("click", iniciarSesion);
+
 
 /*Declaración de variables*/
 /*Seleccionas esa clase del HTML para poder trabajarlo en Javascript*/
@@ -17,20 +16,7 @@ var caja_trasera_login = document.querySelector(".caja_trasera_login");
 var caja_trasera_register = document.querySelector(".caja_trasera_register");
 
 /*59:53*/
-function anchoPagina() {
-  if (window.innerWidth > 850) {
-    caja_trasera_login.style.display = "block";
-    caja_trasera_register.style.display = "block";
-    contenedor_login_register.style.left = "10px";
-  } else {
-    caja_trasera_register.style.display = "block";
-    caja_trasera_register.style.opacity = "1";
-    caja_trasera_login.style.display = "none";
-    formulario_login.style.display = "block";
-    formulario_register.style.display = "none";
-    contenedor_login_register.style.left = "0px";
-  }
-}
+//anchopagina
 /*40:50*/
 function register() {
   if (window.innerWidth > 850) {
@@ -104,6 +90,4 @@ function isValidPassword(password) {
         return true;
     }
 }
-
-anchoPagina();
 
