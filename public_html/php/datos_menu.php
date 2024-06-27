ff2e77b0b566c5027bddb5e1b264daa38f7a52b3
D06R <luisdanielreyesvasquez2000@gmail.com>
2024-06-26 19:26:39 -0500

    Update datos_menu.php

--- public_html/php/datos_menu.php
+++ public_html/php/datos_menu.php
@@ -112,6 +112,96 @@ $menu = [
         'amount' => 1,
         'type' => 'Postre'
     ],
+    [
+        'title' => 'Ají de Gallina',
+        'src' => "assets/img/Ajide_gallina.jpeg",
+        'alt' => 'Ají de Gallina',
+        'description' => 'Suave pollo desmenuzado en una cremosa salsa de ají amarillo y nueces. ¡Un clásico reconfortante!',
+        'stars' => 5,
+        'price' => 25.00,
+        'type' => 'Principal'
+    ],
+    [
+        'title' => 'Arroz con Pato',
+        'src' => "assets/img/Arrozcon_pato.jpg",
+        'alt' => 'Arroz con Pato',
+        'description' => 'Exquisito pato cocido a la perfección con arroz verde y especias. ¡Un deleite del norte del Perú!',
+        'stars' => 5,
+        'price' => 35.00,
+        'type' => 'Principal'
+    ],
+    [
+        'title' => 'Solterito',
+        'src' => "assets/img/Solterito.jpg",
+        'alt' => 'Solterito',
+        'description' => 'Fresca mezcla de choclo, habas y queso fresco. ¡Ideal para los días calurosos!',
+        'stars' => 4,
+        'price' => 15.00,
+        'type' => 'Ensalada'
+    ],
+    [
+        'title' => 'Ensalada de Betarraga',
+        'src' => "assets/img/Betarraga.jpg",
+        'alt' => 'Ensalada de Betarraga',
+        'description' => 'Dulce betarraga cocida con zanahoria y papa, aderezada con mayonesa. ¡Un toque dulce y salado!',
+        'stars' => 4,
+        'price' => 12.00,
+        'type' => 'Ensalada'
+    ],
+    [
+        'title' => 'Papa a la Huancaína',
+        'src' => "assets/img/Huancaina.jpg",
+        'alt' => 'Papa a la Huancaína',
+        'description' => 'Rodajas de papa cubiertas con una cremosa salsa de queso y ají. ¡Irresistiblemente deliciosa!',
+        'stars' => 5,
+        'price' => 12.00,
+        'type' => 'Aperitivo'
+    ],
+    [
+        'title' => 'Tamalitos Verdes',
+        'src' => "assets/img/Tamal_verde.jpeg",
+        'alt' => 'Tamalitos Verdes',
+        'description' => 'Tamales verdes de maíz con cilantro, rellenos de carne. ¡Sabor tradicional en cada bocado!',
+        'stars' => 4,
+        'price' => 10.00,
+        'type' => 'Aperitivo'
+    ],
+    [
+        'title' => 'Emoliente',
+        'src' => "assets/img/Emoliente.png",
+        'alt' => 'Emoliente',
+        'description' => 'Bebida caliente a base de granos tostados de cebada, extractos de hierbas medicinales. ¡Reconfortante y saludable!',
+        'stars' => 4,
+        'price' => 8.00,
+        'type' => 'Bebida'
+    ],
+    [
+        'title' => 'Chicha de Jora',
+        'src' => "assets/img/Jora.jpg",
+        'alt' => 'Chicha de Jora',
+        'description' => 'Maíz germinado, denominado Jora, diferentes frutos y especias aromáticas.',
+        'stars' => 4,
+        'price' => 8.00,
+        'type' => 'Bebida'
+    ],
+    [
+        'title' => 'Mazamorra Morada',
+        'src' => "assets/img/Mazamorra_morada.png",
+        'alt' => 'Mazamorra Morada',
+        'description' => 'Postre con frutas frescas y un toque de canela, tradicional peruano que te sorprenderá.',
+        'stars' => 5,
+        'price' => 10.00,
+        'type' => 'Postre'
+    ],
+    [
+        'title' => 'Picarones',
+        'src' => "assets/img/Mazamorra_morada.png",
+        'alt' => 'Picarones',
+        'description' => 'Irresistibles Picarones bañados en miel de chancaca. El postre perfecto para un dulce momento de placer.',
+        'stars' => 4,
+        'price' => 10.00,
+        'type' => 'Postre'
+    ],
 ];
 
 echo json_encode($menu);
